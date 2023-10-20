a7f393b2468fae79d03caafacd95d4b72dabaad6
Rasmus Kristoffer Pedersen <rasmuspedersen1992@gmail.com>
2023-10-20 17:10:51 +0200

    Implemented poisson-distribution functionality

--- ExcessMortalityFunctions.py
+++ ExcessMortalityFunctions.py
@@ -80,43 +80,14 @@ def removeLeapDays(pdSeries):
 
     return pdSeries.loc[nonLeapDay]
 
-# Function for calculating running mean from surrounding data
-# Assumes input is pandas series, and uses pandas 'rolling' to determine mean
-# Index should be a datetimeindex, with correct dates
-def rnMean(pdSeries,numYears=5,timeResolution='Month'):
-
+def seriesToPivot(pdSeries,timeResolution='Month'):
+    # Helper function for restructuring a pandas series into a pivot-table for rolling-calculations
     if timeResolution == 'Year':
+
         # Start by grouping data by year, in case it's daily or monthly resolution
         # (the min_count flag makes sure that if all are NaN, NaN is actually used instead of 0)
         serYear = pdSeries.groupby(pdSeries.index.year.rename('Year')).sum(min_count=1)
-
-        # Calculate sum of surrounding years and current year
-        curRolling = serYear.rolling(window=(numYears*2)+1,center=True,min_periods=1)
-        curSum = curRolling.sum()
-        curCount = curRolling.count()
-        # Calculate mean of surrounding years by subtracting the current year and dividing by the number of surrounding years
-        # curMean = (curSum - serYear)/(curCount-1)
-        curMean = (curSum - serYear.fillna(0))/(curCount-serYear.notna()*1)
-        
-        # Calculate the sum of squares of surrounding years and current year
-        curSumSqr = serYear.pow(2).rolling(window=(numYears*2)+1,center=True,min_periods=1).sum()
-        # curMeanSqr = (curSumSqr - serYear.pow(2))/(numYears*2)
-        curMeanSqr = (curSumSqr - serYear.pow(2).fillna(0))/(curCount-serYear.notna()*1)
-
-        # Calculate emperical standard deviation 
-        curStd = (curMeanSqr - curMean.pow(2)).pow(0.5)
-        
-        # # Reshape pivottables into series
-        # curMean = curMean.reset_index()
-        # curMean['Date'] = pd.to_datetime(dict(year=curMean.Year,month=np.ones(len(curMean.Year)),day=np.ones(len(curMean.Year))))
-        # curMean = curMean.sort_values('Date').set_index('Date').drop(columns=['Year']).rename(columns={'value':'Baseline'}).iloc[:,0]
-        # curStd = curStd.reset_index()
-        # curStd['Date'] = pd.to_datetime(dict(year=curStd.Year,month=np.ones(len(curStd.Year)),day=np.ones(len(curStd.Year))))
-        # curStd = curStd.sort_values('Date').set_index('Date').drop(columns=['Year']).rename(columns={'value':'StandardDeviation'}).iloc[:,0]
-
-        # curMean = reshapePivot(curMean,timeResolution=timeResolution).rename('Baseline')
-        # curStd  = reshapePivot(curStd,timeResolution=timeResolution).rename('StandardDeviation')
-
+        curPivot = serYear 
 
     elif timeResolution == 'Month':
         # Start by grouping data by month, in case it's on daily resolution
@@ -124,32 +95,6 @@ def rnMean(pdSeries,numYears=5,timeResolution='Month'):
 
         # Organize as pivot table
         curPivot = serMonth.to_frame().pivot_table(serMonth.name,index='Year',columns='Month')
-
-        # Calculate sum of surrounding years and current year
-        curRolling = curPivot.rolling(window=(numYears*2)+1,center=True,min_periods=1)
-        curSum = curRolling.sum()
-        curCount = curRolling.count()
-        # curSum = curPivot.rolling(window=(numYears*2)+1,center=True).sum()
-
-        # Calculate mean of surrounding years by subtracting the current year and dividing by the number of surrounding years
-        # curMean = (curSum - curPivot)/(numYears*2)
-        curMean = (curSum - curPivot.fillna(0))/(curCount-curPivot.notna()*1)
-        
-        # Calculate the sum of squares of surrounding years and current year
-        curSumSqr = curPivot.pow(2).rolling(window=(numYears*2)+1,center=True,min_periods=1).sum()
-        # curMeanSqr = (curSumSqr - curPivot.pow(2))/(numYears*2)
-        curMeanSqr = (curSumSqr - curPivot.pow(2).fillna(0))/(curCount-curPivot.notna()*1)
-
-        # Calculate emperical standard deviation 
-        curStd = (curMeanSqr - curMean.pow(2)).pow(0.5)
-
-        # # Reshape pivottables into series
-        # curMean = curMean.reset_index().melt(id_vars='Year')
-        # curMean['Date'] = pd.to_datetime(dict(year=curMean.Year,month=curMean.Month,day=np.ones(len(curMean.Year))))
-        # curMean = curMean.sort_values('Date').set_index('Date').drop(columns=['Year','Month']).rename(columns={'value':'Baseline'}).iloc[:,0]
-        # curStd = curStd.reset_index().melt(id_vars='Year')
-        # curStd['Date'] = pd.to_datetime(dict(year=curStd.Year,month=curStd.Month,day=np.ones(len(curStd.Year))))
-        # curStd = curStd.sort_values('Date').set_index('Date').drop(columns=['Year','Month']).rename(columns={'value':'StandardDeviation'}).iloc[:,0]
 
     elif timeResolution == 'Day':
         # # Ignore leap day 
@@ -163,62 +108,73 @@ def rnMean(pdSeries,numYears=5,timeResolution='Month'):
         curFrame['Day'] = curFrame.index.day
         
         # Organize as pivot-table (with multi-columns)
-        curPivot = curFrame.pivot_table(values=pdSeries.name,columns=['Month','Day'],index='Year')
+        curPivot = curFrame.pivot_table(values=pdSeries.name,index='Year',columns=['Month','Day'])
 
+    return curPivot
 
-        # Calculate sum of surrounding years and current year
-        curRolling = curPivot.rolling(window=(numYears*2)+1,center=True,min_periods=1)
-        curSum = curRolling.sum() # Get sum of all values in roll
-        curCount = curRolling.count() # Count how many values were used in sum (to avoid counting NaN's)
-        # Calculate mean of surrounding years by subtracting the current year and dividing by the number of surrounding years 
-        # (Replace NaN values with 0. Since the number of Non-NaN values are already counted and used as the divisor, this is fine)
-        # curMean = (curSum - curPivot.fillna(0))/(curCount-1)
-        curMean = (curSum - curPivot.fillna(0))/(curCount-curPivot.notna()*1)
+# Function for calculating running mean from surrounding data
+# Assumes input is pandas series, and uses pandas 'rolling' to determine mean
+# Index should be a datetimeindex, with correct dates
+def rnMean(pdSeries,numYears=5,timeResolution='Month',DistributionType='Standard'):
 
-        # # Calculate sum of surrounding years and current year
-        # curSum = curPivot.rolling(window=(numYears*2)+1,center=True).sum()
-        # # Calculate mean of surrounding years by subtracting the current year and dividing by the number of surrounding years
-        # curMean = (curSum - curPivot)/(numYears*2)
-        
+    # Restructure series into pivottable
+    curPivot = seriesToPivot(pdSeries,timeResolution)
+
+    # Calculate sum of surrounding years and current year
+    curRolling = curPivot.rolling(window=(numYears*2)+1,center=True,min_periods=1)
+    curSum = curRolling.sum() # Get sum of all values in roll
+    curCount = curRolling.count() # Count how many values were used in sum (to avoid counting NaN's)    
+    # Calculate mean of surrounding years by subtracting the current year and dividing by the number of surrounding years 
+    # (Replace NaN values with 0. Since the number of Non-NaN values are already counted and used as the divisor, this is fine)
+    curMean = (curSum - curPivot.fillna(0))/(curCount-curPivot.notna()*1)
+
+    ### Determine uncertainty
+    if DistributionType == 'Standard':
         # Calculate the sum of squares of surrounding years and current year
-        curRollingSqr = curPivot.pow(2).rolling(window=(numYears*2)+1,center=True,min_periods=1)
-        curSumSqr = curRollingSqr.sum()
-        # curMeanSqr = (curSumSqr - curPivot.pow(2).fillna(0))/(curCount-1)
+        curSumSqr = curPivot.pow(2).rolling(window=(numYears*2)+1,center=True,min_periods=1).sum()
         curMeanSqr = (curSumSqr - curPivot.pow(2).fillna(0))/(curCount-curPivot.notna()*1)
-
-
-        # curSumSqr = curPivot.pow(2).rolling(window=(numYears*2)+1,center=True).sum()
-        # curMeanSqr = (curSumSqr - curPivot.pow(2))/(numYears*2)
 
         # Calculate emperical standard deviation 
         curStd = (curMeanSqr - curMean.pow(2).fillna(0)).pow(0.5)
 
+    elif DistributionType == 'Poisson':
+        curSF = pd.DataFrame(poisson.logsf(curPivot,curMean),columns=curPivot.columns,index=curPivot.index)
+
+    # For daily time-resolution, everything is also calculated for leap days in non-leap years. Instead, the average of surrounding days is a better estimate
+    if timeResolution == 'Day':
         # For leap days, use the average of February 28th and March 1st (Leap-days in non-leap-years will be removed below anyways)
         curMean.loc[:,(2,29)] = (curMean.loc[:,(2,28)] + curMean.loc[:,(3,1)])/2
-        curStd.loc[:,(2,29)] = (curStd.loc[:,(2,28)] + curStd.loc[:,(3,1)])/2
-
-        # # Reshape pivottables into series
-        # curMean = curMean.reset_index().melt(id_vars='Year') # Melt pivottable
-        # curMean['Date'] = pd.to_datetime(dict(year=curMean.Year,month=curMean.Month,day=curMean.Day),errors='coerce') # Make a date-columns (coerce "false" leap-days to NaT, i.e. in non-leap years)
-        # curMean = curMean.sort_values('Date').set_index('Date').drop(columns=['Year','Month','Day']).rename(columns={'value':'Baseline'}).iloc[:,0] # Sort by date and drop extra columns
-        # curMean = curMean.loc[curMean.index.notna()] # Remove invalid dates (leap-days in not leap-years)
-
-        # curStd = curStd.reset_index().melt(id_vars='Year') # Melt pivottable
-        # curStd['Date'] = pd.to_datetime(dict(year=curStd.Year,month=curStd.Month,day=curStd.Day),errors='coerce') # Make a date-columns (coerce "false" leap-days to NaT, i.e. in non-leap years)
-        # curStd = curStd.sort_values('Date').set_index('Date').drop(columns=['Year','Month','Day']).rename(columns={'value':'StandardDeviation'}).iloc[:,0] # Sort by date and drop extra columns
-        # curStd = curStd.loc[curStd.index.notna()] # Remove invalid dates (leap-days in not leap-years)
-
+        
+        if DistributionType == 'Standard':
+            curStd.loc[:,(2,29)] = (curStd.loc[:,(2,28)] + curStd.loc[:,(3,1)])/2
+        elif DistributionType == 'Poisson':
+            curSF.loc[:,(2,29)] = (curSF.loc[:,(2,28)] + curSF.loc[:,(3,1)])/2
 
     # Reshape pivottables into series
     curMean = reshapePivot(curMean,timeResolution=timeResolution).rename('Baseline')
-    curStd  = reshapePivot(curStd,timeResolution=timeResolution).rename('StandardDeviation')
+    
+    if DistributionType == 'Standard':
+        curStd  = reshapePivot(curStd,timeResolution=timeResolution).rename('StandardDeviation')
+    elif DistributionType == 'Poisson':
+        curSF  = reshapePivot(curSF,timeResolution=timeResolution).rename('LogSurvivalFunction')
 
-    return curMean,curStd 
+    if DistributionType == 'Standard':
+        return curMean,curStd 
+    elif DistributionType == 'Poisson':
+        return curMean,curSF
 
-def getExcessAndZscore(pdSeries,curMean,curStd):
+def getPoissonIntervals(intervalValue,curBase):
+    # Helper function for getting the probability intervals when assuming a poisson distribution
+    # Calculates the top and bottom of the "inner" interval, and returns it as a series with same indices as the baseline
+    curBot,curTop = poisson.interval(intervalValue,curBase)
+    curBot = pd.Series(curBot,index=curBase.index)
+    curTop = pd.Series(curTop,index=curBase.index)
+    return curBot,curTop 
+
+def getExcessAndZscore(pdSeries,curBase,curStd):
 
     # Calculate excess as difference between mean and data
-    curExc = pdSeries - curMean 
+    curExc = pdSeries - curBase 
     # And Z-score as excess in terms of standard deviations 
     curZsc = curExc / curStd 
 
